08e1bb96053738b9fecad28f20feb1c6ef5a88f0
Jesus R. Gonzalez J <JGONZ@MacBook-Pro-JGONZ.local>
2020-09-15 11:06:01 -0300

    15/09/2020

--- question2.js
+++ question2.js
@@ -1,5 +1,19 @@
+/* const list = ['best', 'company', 'ever']
+
+let rokket = list.reduce((r, e) => r.length < e.length ? e : r, "");
+
+console.log(rokket); */
+
 const list = ['best', 'company', 'ever']
 
-const rokket = list.indexOf(0);
+function rokket(list) {
+    let longest = list[0];
+    for (let i = 1; i < list.length; i++) {
+        if (list[i].length > longest.length) {
+            longest = list[i];
+        }
+    }
+    return longest;
+}
 
-console.log(rokket);
+console.log(rokket(list));
